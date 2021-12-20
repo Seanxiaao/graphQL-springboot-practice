5817ac41e84c6a55576ef7accc891272b8cda182
Ye Xiao <287864960@qq.com>
2021-12-20 14:17:43 -0800

    feat: maven settle up

---
+++ src/main/java/com/seanxiaao/springbootwithgraphql/SpringbootWithGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.seanxiaao.springbootwithgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWithGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seanxiaao/springbootwithgraphql/SpringbootWithGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanxiaao.springbootwithgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
